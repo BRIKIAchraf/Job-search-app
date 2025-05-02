3e3942f806bb04467668e0207d4963f0e51ccbad
ashraf breki <brikiachraf@DJERBA.r-iset.tn>
2025-05-02 23:10:51 +0200

    feat: implement all the router of the external tools and implement the loading of the favorites and the layout of the mainlayout

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,5 @@
+import { auth } from "@/app/utils/auth";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
