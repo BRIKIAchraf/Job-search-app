44d807fac62ad4723246cffb22886528bb8fc976
ashraf breki <brikiachraf@DJERBA.r-iset.tn>
2025-05-15 15:25:04 +0200

    fix : update the next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // ✅ Fixes the "next export" removal issue
   images: {
     remotePatterns: [
       {
